4f8d187c8a8484e60fa02b89162787f518fea27b
begenov <begenovorazali@icloud.com>
2023-05-16 16:26:54 +0600

    manager token create

---
+++ pkg/auth/manager.go
@@ -0,0 +1,69 @@
+package auth
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type TokenManager interface {
+}
+
+type Manager struct {
+	signinKey string
+}
+
+func NewManager(signinKey string) (TokenManager, error) {
+	if signinKey == "" {
+		return nil, errors.New("empty signing key")
+	}
+
+	return &Manager{
+		signinKey: signinKey,
+	}, nil
+}
+
+func (m *Manager) NewJWT(studentID string, ttl time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(ttl).Unix(),
+		Subject:   studentID,
+	})
+
+	return token.SignedString([]byte(m.signinKey))
+}
+
+func (m *Manager) Parse(accessToken string) (string, error) {
+	token, err := jwt.Parse(accessToken, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+		return []byte(m.signinKey), nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", fmt.Errorf("error get user claims from token")
+	}
+
+	return claims["sub"].(string), nil
+
+}
+
+func NewRefreshToken() (string, error) {
+	b := make([]byte, 32)
+
+	s := rand.NewSource(time.Now().Unix())
+	r := rand.New(s)
+
+	if _, err := r.Read(b); err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%x", b), nil
+
+}
