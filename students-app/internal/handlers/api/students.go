bade9033cd185f630c418c08bc637f05a478d804
begenov <begenovorazali@icloud.com>
2023-05-16 12:34:58 +0600

    student init router

--- students-app/internal/handlers/api/students.go
+++ students-app/internal/handlers/api/students.go
@@ -5,6 +5,20 @@ import "github.com/gin-gonic/gin"
 func (h *Handler) initStudentsRoutes(api *gin.RouterGroup) {
 	students := api.Group("/students")
 	{
-		students.GET("")
+		students.POST("/sign-up", h.studentSignUp)
+		students.POST("/sig-in", h.studentSingIn)
+		auth := students.Group("/") // TODO middleaware
+		{
+			auth.PUT("/update/:id")
+			auth.DELETE("/delete/:id")
+			auth.GET("/:id/courses")
+		}
 	}
+}
+
+func (h *Handler) studentSignUp(ctx *gin.Context) {
+}
+
+func (h *Handler) studentSingIn(ctx *gin.Context) {
+
 }
