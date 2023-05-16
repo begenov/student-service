e0380d2cd846647e1a60b1a672eb9ab6f603010e
begenov <begenovorazali@icloud.com>
2023-05-16 12:41:30 +0600

    update services student

--- students-app/internal/services/students.go
+++ students-app/internal/services/students.go
@@ -1,6 +1,10 @@
 package services
 
-import "github.com/begenov/test-task-backend/students-app/internal/storage"
+import (
+	"context"
+
+	"github.com/begenov/test-task-backend/students-app/internal/storage"
+)
 
 type StudentsService struct {
 	storage storage.Students
@@ -10,4 +14,20 @@ func NewStudentsService(storage storage.Students) *StudentsService {
 	return &StudentsService{
 		storage: storage,
 	}
+}
+
+func (s *StudentsService) studentSignUp(ctx context.Context) {
+}
+
+func (s *StudentsService) studentSingIn(ctx context.Context) {
+}
+
+func (s *StudentsService) studentUpdate(ctx context.Context) {
+}
+
+func (s *StudentsService) studentDelete(ctx context.Context) {
+}
+
+func (s *StudentsService) studentByIDCourses(ctx context.Context) {
+
 }
