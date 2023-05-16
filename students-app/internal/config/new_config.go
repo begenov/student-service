a7dd4765b2211cf4a0f1530eda472c69df12e0a1
begenov <begenovorazali@icloud.com>
2023-05-16 11:53:24 +0600

    update config file

--- students-app/internal/config/new_config.go
+++ students-app/internal/config/new_config.go
@@ -2,9 +2,18 @@ package config
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/subosito/gotenv"
+)
+
+const (
+	defaultServerPort               = "8000"
+	defaultServerRWTimeout          = 10 * time.Second
+	defaultServerMaxHeaderMegabytes = 1
+	defaultAccessTokenTTL           = 15 * time.Minute
+	defaultRefreshTokenTTL          = 24 * time.Hour * 30
 )
 
 type Config struct {
@@ -19,6 +28,12 @@ type Config struct {
 		Driver string
 		DSN    string
 	}
+
+	JWT struct {
+		AccessTokenTTL  time.Duration
+		RefreshTokenTTL time.Duration
+		SigningKey      string
+	}
 }
 
 func NewConfig(path string) (*Config, error) {
@@ -26,5 +41,38 @@ func NewConfig(path string) (*Config, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to load environment variables from file: %v", err)
 	}
-	return nil, nil
+
+	driver := os.Getenv("")
+	dsn := os.Getenv("")
+	jwtKey := os.Getenv("")
+
+	return &Config{
+		Server: struct {
+			Port               string
+			ReadTimeout        time.Duration
+			WriteTimeout       time.Duration
+			MaxHeaderMegabytes int
+		}{
+			Port:               defaultServerPort,
+			ReadTimeout:        defaultServerRWTimeout,
+			WriteTimeout:       defaultServerRWTimeout,
+			MaxHeaderMegabytes: defaultServerMaxHeaderMegabytes,
+		},
+		Database: struct {
+			Driver string
+			DSN    string
+		}{
+			Driver: driver,
+			DSN:    dsn,
+		},
+		JWT: struct {
+			AccessTokenTTL  time.Duration
+			RefreshTokenTTL time.Duration
+			SigningKey      string
+		}{
+			AccessTokenTTL:  defaultAccessTokenTTL,
+			RefreshTokenTTL: defaultRefreshTokenTTL,
+			SigningKey:      jwtKey,
+		},
+	}, nil
 }
