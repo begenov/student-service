3a6355c846fa2492488668edc219167b2b93f8f8
begenov <begenovorazali@icloud.com>
2023-05-16 10:52:27 +0600

    handlers students

--- students-app/internal/handlers/new_handler.go
+++ students-app/internal/handlers/new_handler.go
@@ -1,1 +1,22 @@
 package handlers
+
+import (
+	"github.com/begenov/test-task-backend/students-app/internal/config"
+	"github.com/begenov/test-task-backend/students-app/internal/services"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	services *services.Services
+}
+
+func NewHandler(services *services.Services) *Handler {
+	return &Handler{
+		services: services,
+	}
+}
+
+func (h *Handler) Init(cfg *config.Config) *gin.Engine {
+	router := gin.Default()
+	return router
+}
