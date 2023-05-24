999161d6e5c4ff2f3038515f943a8868b6fa55bb
begenov <begenovorazali@icloud.com>
2023-05-24 18:16:16 +0600

    update students

--- internal/domain/student.go
+++ internal/domain/student.go
@@ -1,11 +1,12 @@
 package domain
 
 type Student struct {
-	ID      int      `json:"id"`
-	Email   string   `json:"email" binding:"required,email,max=64"`
-	Name    string   `json:"name" binding:"required,min=3,max=64"`
-	GPA     float64  `json:"gpa" binding:"required"`
-	Courses []string `json:"courses"`
+	ID       int      `json:"id"`
+	Email    string   `json:"email" binding:"required,email,max=64"`
+	Name     string   `json:"name" binding:"required,min=3,max=64"`
+	Password string   `json:"password" binding:"required,min=8,max=64"`
+	GPA      float64  `json:"gpa" binding:"required"`
+	Courses  []string `json:"courses"`
 }
 
 type UpdateStudentInput struct {
