0238aa0511d16e2c576b62b5d72ea909b3b5e807
begenov <begenovorazali@icloud.com>
2023-05-24 16:32:19 +0600

    init student test services

---
+++ internal/service/students_test.go
@@ -0,0 +1,26 @@
+package service
+
+import (
+	"testing"
+
+	mock_service "github.com/begenov/student-service/internal/service/mocks"
+)
+
+func MockStudentService(t *testing.T) (*StudentService, *mock_service.MockStudents) {
+	return nil, nil
+}
+
+func TestStudentService_Create(t *testing.T) {
+
+}
+
+func TestStudentService_GetStudentByID(t *testing.T) {
+
+}
+
+func TestStudentService_Update(t *testing.T) {
+
+}
+
+func TestStudentService_Delete(t *testing.T) {
+}
