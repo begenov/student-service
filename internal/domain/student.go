42fb80fdf0f1e296d24d1bc8d943ceee5c604d0f
begenov <begenovorazali@icloud.com>
2023-05-26 18:01:42 +0600

    update domain student

--- internal/domain/student.go
+++ internal/domain/student.go
@@ -1,13 +1,16 @@
 package domain
 
+import "time"
+
 type Student struct {
-	ID       int      `json:"id"`
-	Email    string   `json:"email" binding:"required,email,max=64"`
-	Name     string   `json:"name" binding:"required,min=3,max=64"`
-	Password string   `json:"password" binding:"required,min=8,max=64"`
-	GPA      float64  `json:"gpa" binding:"required"`
-	Courses  []string `json:"courses"`
-	Session
+	ID           int       `json:"id"`
+	Email        string    `json:"email" binding:"required,email,max=64"`
+	Name         string    `json:"name" binding:"required,min=3,max=64"`
+	Password     string    `json:"password" binding:"required,min=8,max=64"`
+	GPA          float64   `json:"gpa" binding:"required"`
+	Courses      []string  `json:"courses"`
+	RefreshToken string    `json:"-"`
+	ExpiresAt    time.Time `json:"-"`
 }
 
 type UpdateStudentInput struct {
