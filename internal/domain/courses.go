70042ccfe2c4a081b99ca09ee69e1e2d1dc3b027
begenov <begenovorazali@icloud.com>
2023-05-27 10:54:09 +0600

    update domain

--- internal/domain/courses.go
+++ internal/domain/courses.go
@@ -1,7 +1,16 @@
 package domain
 
+import "time"
+
 type Courses struct {
-	ID       int      `json:"id"`
-	Name     string   `json:"name"`
-	Students []string `json:"students"`
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"-"`
+	UpdatedAt   time.Time `json:"-"`
+	Students    []string  `json:"students"`
+}
+
+type Response struct {
+	Courses []Courses `json:"courses"`
 }
